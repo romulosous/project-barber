6307abc9b94f4a8c1d848232b9a7789e81c59a97
Romulo Carvalho <romulocarvalho081@gmail.com>
2024-06-18 15:36:50 -0300

    feat: add ServiceItem component

--- app/(home)/_components/barbershop-item.tsx
+++ app/(home)/_components/barbershop-item.tsx
@@ -1,25 +1,35 @@
+"use client"
 import { Badge } from "@/app/_components/ui/badge";
 import { Button } from "@/app/_components/ui/button";
 import { Card, CardContent } from "@/app/_components/ui/card";
 import { Barbershop } from "@prisma/client";
 import { StarIcon } from "lucide-react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 interface BarbershopItemProps {
   barbershop: Barbershop;
 }
 
 const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
+  const router = useRouter();
+
+  const handleBookingClick = () => {
+    router.push(`/barbershops/${barbershop.id}`);
+  };
   return (
     <Card className="min-w-[167px] max-w-[167px] rounded-2xl">
       <CardContent className="px-1 py-0">
         <div className="h-[159px] w-full relative">
-            <div className="absolute top-2 left-2 z-50">
-                <Badge variant="secondary" className="opacity-90 flex gap-1 items-center ">
-                    <StarIcon size={12} className="fill-primary text-primary"/>
-                    <span className="text-xs">5,0</span>
-                </Badge>
-            </div>
+          <div className="absolute top-2 left-2 z-50">
+            <Badge
+              variant="secondary"
+              className="opacity-90 flex gap-1 items-center "
+            >
+              <StarIcon size={12} className="fill-primary text-primary" />
+              <span className="text-xs">5,0</span>
+            </Badge>
+          </div>
           <Image
             src={barbershop.imageUrl}
             alt={`${barbershop.name} Image`}
@@ -36,7 +46,7 @@ const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
           <p className="text-sm text-gray-400 overflow-hidden text-ellipsis text-nowrap">
             {barbershop.address}
           </p>
-          <Button className="w-full mt-3" variant="secondary">
+          <Button onClick={handleBookingClick} className="w-full mt-3" variant="secondary">
             Reservar
           </Button>
         </div>
