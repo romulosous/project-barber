5a3dd198c0510894b3a0ec35d6147de84e7159bc
Romulo Carvalho <romulocarvalho081@gmail.com>
2024-06-18 22:18:53 -0300

    feat: ensure unauthenticated user is redirected to login on booking click

--- app/barbershops/[id]/_components/barbershop-info.tsx
+++ app/barbershops/[id]/_components/barbershop-info.tsx
@@ -15,8 +15,9 @@ const BarbershopInfo = ({ barbershop }: BarbershopDetailsPageProps) => {
   const router = useRouter();
 
   const handleBackClick = () => {
-    router.back();
+    router.replace("/")
   };
+  
   return (
     <div>
       <div className="h-[250px] w-full relative">
