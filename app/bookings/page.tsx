937e8535e6d4e856ec70b3d444ba493b90f1725a
Romulo Carvalho <romulocarvalho081@gmail.com>
2024-06-30 12:02:08 -0300

    fix: ensure auth options are exported from _lib folder

--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -1,10 +1,9 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
 import Header from "../_components/header";
 import { redirect } from "next/navigation";
 import { db } from "../_lib/prisma";
 import BookingItem from "../_components/booking-item";
-import { isFuture, isPast, isToday } from "date-fns";
+import { authOptions } from "../_lib/auth";
 
 const BookingsPage = async () => {
   const session = await getServerSession(authOptions);
